648c839433f6ec64e8b6d5d97f1d2cebc6cb34e7
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-30 17:32:56 +0300

    list products by categories

--- pages/categories/_slug.vue
+++ pages/categories/_slug.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="container">
-    <p>Products for category</p>
+  <div class="section">
+    <div class="container is-fluid">
+      <div class="columns is-multiline">
+        <div
+          class="column is-3"
+          v-for="product in products"
+          :key="product.slug"
+        >
+          <Product :product="product" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import Product from "../../components/products/Product";
+export default {
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  async asyncData({ params, app }) {
+    const response = await app.$axios.$get(`products?category=${params.slug}`);
+    return {
+      products: response.data,
+    };
+  },
+};
+</script>
