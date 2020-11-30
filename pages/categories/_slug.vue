84e3527fd9be182d2762d33cefc147cc2b55f839
Luteranya Nyange <dieume.dev@gmail.com>
2020-11-30 15:36:10 +0300

    categories vue

---
+++ pages/categories/_slug.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <p>Products for category</p>
+  </div>
+</template>
--- store/index.js
+++ store/index.js
@@ -17,9 +17,7 @@ export const mutations = {
 export const actions = {
     async nuxtServerInit({ commit }, { $axios }) {
         let response = await $axios.$get('categories');
-        // console.log('data', response.data);
         commit('SET_CATEGORIES', response.data);
-        // const response = await $axios.$get('/categories');
     }
 
 }
