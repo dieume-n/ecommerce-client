ff1c48b2a5898640957c6efd1cad048f1ae400b9
Luteranya Nyange <dieume.dev@gmail.com>
2021-01-04 14:12:30 +0300

    switch shipping address

--- components/checkout/addresses/ShippingAddress.vue
+++ components/checkout/addresses/ShippingAddress.vue
@@ -2,30 +2,59 @@
   <article class="message">
     <div class="message-body">
       <h1 class="title is-5">Ship to</h1>
-      <template v-if="selectedAddress">
-        <p>{{ selectedAddress.name }}</p>
-        <p>{{ selectedAddress.address_1 }}</p>
-        <p v-if="selectedAddress.address_2">{{ selectedAddress.address_2 }}</p>
-        <p>{{ selectedAddress.city }}</p>
-        <p>{{ selectedAddress.postal_code }}</p>
-        <p>{{ selectedAddress.country.name }}</p>
-      </template>
+      <div v-if="selecting">
+        <ShippingAddressSelector
+          :addresses="addresses"
+          :selectedAddress="selectedAddress"
+          @click="addressSelected"
+        />
+      </div>
+      <div v-else>
+        <template v-if="selectedAddress">
+          <div>
+            <p>{{ selectedAddress.name }}</p>
+            <p>{{ selectedAddress.address_1 }}</p>
+            <p v-if="selectedAddress.address_2">
+              {{ selectedAddress.address_2 }}
+            </p>
+            <p>{{ selectedAddress.city }}</p>
+            <p>{{ selectedAddress.postal_code }}</p>
+            <p>{{ selectedAddress.country.name }}</p>
+          </div>
+        </template>
+        <div class="mt-2 field is-grouped">
+          <p class="control">
+            <a
+              href="#"
+              class="button is-info"
+              @click.prevent="selecting = true"
+            >
+              Change shipping address
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   </article>
 </template>
 <script>
+import ShippingAddressSelector from "@/components/checkout/addresses/ShippingAddressSelector";
 export default {
-  data() {
-    return {
-      localAddresses: this.addresses,
-      selectedAddress: null,
-    };
+  components: {
+    ShippingAddressSelector,
   },
   props: {
     addresses: {
       required: true,
       type: Array,
     },
+  },
+  data() {
+    return {
+      selecting: false,
+      localAddresses: this.addresses,
+      selectedAddress: null,
+    };
   },
   computed: {
     defaultAddress() {
@@ -35,6 +64,10 @@ export default {
   methods: {
     switchAddress(address) {
       this.selectedAddress = address;
+    },
+    addressSelected(address) {
+      this.switchAddress(address);
+      this.selecting = false;
     },
   },
   created() {
---
+++ components/checkout/addresses/ShippingAddressSelector.vue
@@ -0,0 +1,46 @@
+<template>
+  <table class="table is-hoverable is-fullwidth">
+    <tbody>
+      <tr v-for="address in addresses" :key="address.id">
+        <td class="p-4">
+          <div
+            :class="{
+              'has-text-weight-bold': address.id === selectedAddress.id,
+            }"
+          >
+            <p>{{ address.name }}</p>
+            <p>{{ address.address_1 }}</p>
+            <p v-if="address.address_2">
+              {{ address.address_2 }}
+            </p>
+            <p>{{ address.city }}</p>
+            <p>{{ address.postal_code }}</p>
+            <p>{{ address.country.name }}</p>
+          </div>
+        </td>
+        <td class="p-4">
+          <button
+            class="button is-info"
+            @click.prevent="$emit('click', address)"
+          >
+            Ship here
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  props: {
+    addresses: {
+      required: true,
+      type: Array,
+    },
+    selectedAddress: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
